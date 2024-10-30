cb41e125614ec33d7036df30d880255396531597
Andres <aterlag0912@g.educaand.es>
2024-10-30 08:42:02 +0100

    Verswion inicial

---
+++ projecto.java/src/es/iesoretania/proyecto/eclipse/datos/Prueba.java
@@ -0,0 +1,23 @@
+package es.iesoretania.proyecto.eclipse.datos;
+
+public class Prueba {
+					
+public static void main(String[] args) {
+	
+	
+	
+System.out.println("Hola mundo");
+
+System.out.println("Hola mundo");
+
+System.out.println("Adios");
+
+
+
+
+
+
+}
+
+}
+
---
+++ projecto.java/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module projecto.java {
+}
